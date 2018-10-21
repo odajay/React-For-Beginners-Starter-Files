586ef252152d49ac5f0bd79dc80f1b4225c39416
Jonathan Gall <odajay@gmail.com>
2018-10-21 02:05:18 +0200

    Finishing chapter 13 on states

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,12 @@
 import React from "react";
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div className="inventory">Inventory!!!
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
       </div>
 
     );
