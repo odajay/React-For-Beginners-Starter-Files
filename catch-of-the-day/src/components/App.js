28d3b51338c3655c72464e2ee7413fd97878c724
Jonathan Gall <odajay@gmail.com>
2018-10-21 04:04:02 +0200

    Finishing chapter 15 on displaying state and using ES6 variables to set multiples variables in one-shot

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+
 class App extends React.Component {
   state = {
     fishes: {},
@@ -25,7 +27,10 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" />      
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>     
         </div>
           <Order />
           <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
